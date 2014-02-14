7ef99e91f9cfbe7a48b1ad22ddd4469ece156db6
brl <bruce@subgraph.com>
2014-02-14 11:58:15 -0500

    Method added to obtain list of public keys.  More source constants added.

--- projects/sgmail/src/com/subgraph/sgmail/identity/PublicIdentity.java
+++ projects/sgmail/src/com/subgraph/sgmail/identity/PublicIdentity.java
@@ -1,16 +1,20 @@
 package com.subgraph.sgmail.identity;
 
-import java.util.List;
-
+import org.bouncycastle.openpgp.PGPPublicKey;
 import org.bouncycastle.openpgp.PGPPublicKeyRing;
 
+import java.util.List;
+
 public interface PublicIdentity {
-	final static int KEY_SOURCE_LOCAL_KEYRING = 1;
-	final static int KEY_SOURCE_MANUAL_IMPORT = 2;
-	final static int KEY_SOURCE_PUBLIC_KEYSERVER = 3;
+    final static int KEY_SOURCE_USER_GENERATED = 1;
+	final static int KEY_SOURCE_LOCAL_KEYRING = 2;
+    final static int KEY_SOURCE_IDENTITY_SERVER = 3;
+	final static int KEY_SOURCE_MANUAL_IMPORT = 4;
+	final static int KEY_SOURCE_PUBLIC_KEYSERVER = 5;
 	
 	int getKeySource();
 	PGPPublicKeyRing getPGPPublicKeyRing();
-	byte[] getImageBytes();
+    List<PGPPublicKey> getPublicKeys();
+	byte[] getImageData();
 	List<String> getUserIds();
 }
