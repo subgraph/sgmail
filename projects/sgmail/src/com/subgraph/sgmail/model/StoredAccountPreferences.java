ec73849a244422877fcd8e6f37e4cf2330753d88
brl <bruce@subgraph.com>
2014-02-14 12:02:48 -0500

    added factory constructor

--- projects/sgmail/src/com/subgraph/sgmail/model/StoredAccountPreferences.java
+++ projects/sgmail/src/com/subgraph/sgmail/model/StoredAccountPreferences.java
@@ -2,14 +2,20 @@ package com.subgraph.sgmail.model;
 
 public class StoredAccountPreferences extends StoredPreferences {
 	
+	public static StoredAccountPreferences create(Account account, Model model) {
+		final StoredAccountPreferences preferences = new StoredAccountPreferences(account, model);
+		Preferences.initializeAccountPreferences(preferences);
+		return preferences;
+	}
+
 	final Account account;
 	
-	StoredAccountPreferences(Account account, Model model) {
+	private StoredAccountPreferences(Account account, Model model) {
 		this.account = account;
 		this.model = model;
 	}
 	
-	Account getAccount() {
+	public Account getAccount() {
 		return account;
 	}
 }
