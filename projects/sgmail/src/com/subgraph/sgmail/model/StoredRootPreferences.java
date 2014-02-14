eb307b8d5502ecb5812097050e5f66346ae2e7f7
brl <bruce@subgraph.com>
2014-02-14 12:03:55 -0500

    added factory constructor to initialize defaults

--- projects/sgmail/src/com/subgraph/sgmail/model/StoredRootPreferences.java
+++ projects/sgmail/src/com/subgraph/sgmail/model/StoredRootPreferences.java
@@ -2,5 +2,9 @@ package com.subgraph.sgmail.model;
 
 
 public class StoredRootPreferences extends StoredPreferences {
-
+	public static StoredRootPreferences create() {
+		final StoredRootPreferences prefs = new StoredRootPreferences();
+		Preferences.initializeRootPreferences(prefs);
+		return prefs;
+	}
 }
