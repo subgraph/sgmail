8a09c91879a8d028a7435f87a976944dcfe7f584
brl <bruce@subgraph.com>
2014-02-16 19:26:50 -0500

    Use the regular text control since StyledText has performance issues

--- projects/sgmail/src/com/subgraph/sgmail/ui/panes/right/MessageBodyViewer.java
+++ projects/sgmail/src/com/subgraph/sgmail/ui/panes/right/MessageBodyViewer.java
@@ -6,18 +6,29 @@ import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 
 import com.subgraph.sgmail.ui.MessageBodyUtils;
+import org.eclipse.swt.widgets.Text;
 
 public class MessageBodyViewer extends Composite {
 
 	public MessageBodyViewer(Composite parent, Message message) {
 		super(parent, SWT.NONE);
-		setLayout(new FillLayout());
-		StyledText text = new StyledText(this, SWT.WRAP | SWT.READ_ONLY);
-		text.setMargins(26, 10, 10, 10);
+
+        setBackground(getDisplay().getSystemColor(SWT.COLOR_WHITE));
+        GridLayout layout = new GridLayout();
+        layout.marginLeft = 26;
+        layout.marginRight = 10;
+        layout.marginTop = 10;
+        layout.marginBottom = 10;
+        setLayout(layout);
+
+        Text text = new Text(this, SWT.WRAP | SWT.READ_ONLY);
 		text.setFont(JFaceResources.getTextFont());
+        text.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		
 		final String body = MessageBodyUtils.getTextBody(message);
 		if(body != null) {
