17fbd917b84da549ff53e51b6d0ab7965e021f66
brl <bruce@subgraph.com>
2014-03-24 03:43:29 -0400

    Builder added to public API

--- projects/sgmail/src/com/subgraph/sgmail/messages/MessageUser.java
+++ projects/sgmail/src/com/subgraph/sgmail/messages/MessageUser.java
@@ -1,7 +1,22 @@
 package com.subgraph.sgmail.messages;
 
+import com.subgraph.sgmail.messages.impl.MessageUserImpl;
+
 public interface MessageUser {
-    enum UserType { USER_SENDER, USER_TO, USER_CC_}
+    static MessageUser createSender(String username, String address) {
+        return new MessageUserImpl(username, address, UserType.USER_SENDER);
+    }
+    static MessageUser createTo(String username, String address) {
+        return new MessageUserImpl(username, address, UserType.USER_TO);
+    }
+    static MessageUser createCC(String username, String address) {
+        return new MessageUserImpl(username, address, UserType.USER_CC);
+    }
+    static MessageUser create(String username, String address, UserType type) {
+        return new MessageUserImpl(username, address, type);
+    }
+
+    enum UserType { USER_SENDER, USER_TO, USER_CC }
     String getUsername();
     String getAddress();
     UserType getType();
--- projects/sgmail/src/com/subgraph/sgmail/messages/StoredIMAPMessage.java
+++ projects/sgmail/src/com/subgraph/sgmail/messages/StoredIMAPMessage.java
@@ -1,7 +1,10 @@
 package com.subgraph.sgmail.messages;
 
+import com.subgraph.sgmail.messages.impl.StoredIMAPMessageBuilder;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.List;
 
 public interface StoredIMAPMessage extends StoredMessage {
     StoredIMAPFolder getIMAPFolder();
@@ -9,4 +12,22 @@ public interface StoredIMAPMessage extends StoredMessage {
     int getMessageNumber();
     void setMessageNumber(int value);
     MimeMessage toMimeMessage() throws MessagingException;
+
+    interface Builder {
+        static Builder create(byte[] rawBytes) {
+            return new StoredIMAPMessageBuilder(rawBytes);
+        }
+        Builder messageUID(long value);
+        Builder subject(String value);
+        Builder displayText(String value);
+        Builder uniqueMessageId(long value);
+        Builder conversationId(long value);
+        Builder messageDate(long value);
+        Builder flags(long value);
+        Builder sender(MessageUser value);
+        Builder recipients(List<MessageUser> value);
+        Builder attachments(List<MessageAttachment> value);
+        Builder labels(List<StoredMessageLabel> value);
+        StoredIMAPMessage build();
+    }
 }
